690dc8e7b301952125c2130ce042efd32db48e77
Amber Mohler <ambermohler@imac.lan>
2019-12-03 16:52:04 -0500

    initial commit

--- js/index.js
+++ js/index.js
@@ -1,1 +1,33 @@
-// Your code goes here
+/*doubleclick
+
+click
+
+resize
+
+scroll
+
+load
+
+wheel
+
+keydown
+
+Select
+
+mouse enter 
+
+mouse leave*/
+
+
+
+
+document.querySelectorAll('.btn').forEach(element => {
+    element.addEventListener('click', () => {
+        element.style.backgroundColor = 'greenyellow';
+        element.style.fontFamily = 'impact';
+        element.style.fontSize = '3rem';
+    })
+})
+
+
+
